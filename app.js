a338338e4155627fc6fde275c5de7721da3ae91e
mailomwangi <15156752+bencracocu@users.noreply.github.com>
2020-04-07 16:21:29 +0300

    troubleshoot npm not starting

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   //This string is what will keep user's cookies safe from compromise
-  secret: 'NFSmIxwHPx050J2c84IeIpVlSBk0TR32';
+  secret: 'NFSmIxwHPx050J2c84IeIpVlSBk0TR32',
   resave: true,
   saveUninitialized: false
 }));
